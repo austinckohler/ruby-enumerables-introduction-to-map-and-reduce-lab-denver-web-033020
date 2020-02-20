60ca094bdc40dccafad9c73928bc0559cc446dc3
Austin Kohler <austinckohler@gmail.com>
2020-02-20 22:23:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,12 +10,12 @@ end
 
 def map_to_no_change(source_array)
   i = 0 
-  negative_array = []
+  final_array = []
   while i < source_array.length do
-   negative_array.push(source_array)
+   final_array.push
     i += 1 
 end
-negative_array
+final_array
 end
 
 def map_to_double(source_array)
