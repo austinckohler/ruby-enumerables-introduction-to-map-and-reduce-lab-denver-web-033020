fbed0570d82718d7bc14fbb00e358e8c240e0d22
Austin Kohler <austinckohler@gmail.com>
2020-02-20 22:16:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,12 @@ end
 negative_array
 end
 
+def map_to_no_change(source_array)
+  i = 0 
+  negative_array = []
+  while i < source_array.length do
+   negative_array.push(source_array[i] )
+    i += 1 
+end
+negative_array
+end
