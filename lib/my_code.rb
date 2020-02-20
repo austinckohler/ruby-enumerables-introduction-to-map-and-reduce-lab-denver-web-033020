089ffbe87030802622cb948e2318bb7283fc4ad6
Austin Kohler <austinckohler@gmail.com>
2020-02-20 22:17:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,3 +17,23 @@ def map_to_no_change(source_array)
 end
 negative_array
 end
+
+def map_to_double(source_array)
+  i = 0 
+  negative_array = []
+  while i < source_array.length do
+   negative_array.push(source_array)
+    i += 1 
+end
+negative_array
+end
+
+def map_to_square(source_array)
+  i = 0 
+  negative_array = []
+  while i < source_array.length do
+   negative_array.push(source_array)
+    i += 1 
+end
+negative_array
+end
